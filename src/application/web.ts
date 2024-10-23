10e4d02a7e2b2f64ff065150e7e90a9c74ab6186
erisatrio1 <erisatrio03@gmail.com>
2024-10-23 16:55:02 +0700

    add: cors

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express, {Request} from "express";
 import { errorMiddleware } from "../middleware/error-middleware";
 import { publicRouter } from "../route/public-api";
 import { apiRouter } from "../route/api";
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 export const web = express();
+web.use(cors<Request>());
 web.use(express.json());
 web.use(cookieParser());
 web.use(publicRouter);
